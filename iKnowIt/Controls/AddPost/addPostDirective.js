cb912d7e1ccb6ff1a4690530521a5a25e42400af
Guille <gleguia@gmail.com>
2015-08-07 17:03:37 -0500

    test

---
+++ iKnowIt/Controls/AddPost/addPostDirective.js
@@ -0,0 +1,14 @@
+app.directive('addPost', [function () {
+    return {
+        restrict: "E",
+        templateUrl: "../Controls/AddPost/addPostTemplate.html",
+        scope: {
+           
+        },
+        link: function (scope, element, attributes) {
+          
+            
+
+        }
+    }
+}]);

2a7b1a695dbf07e1f25ba282b7f935bef62bedf2
Ginoluis83 <gilu.carrascomori@gmail.com>
2015-08-07 16:36:00 -0500

    App container added

--- iKnowIt/controllers/mainController.js
+++ iKnowIt/controllers/mainController.js
@@ -1,13 +1,19 @@
 ﻿app.controller('mainController', ['$scope', function ($scope) {
 
+    var getMockupContent = function () {
+        var content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 
-    $scope.message = '';
+        return content;
+    }
+
+    $scope.title = 'IKnowIT - Knowledge Database';
 
     $scope.posts = [
-        { title: 'titulo 1', status: '', content: '', date: '' },
-        { title: 'titulo 2', status: '', content: '', date: '' },
-        { title: 'titulo 3', status: '', content: '', date: '' },
-        { title: 'titulo 4', status: '', content: '', date: '' }
+        { title: 'How to upload files using JQuery', type: 'Article', status: -1, content: getMockupContent(), date: new Date(), tags: [] },
+        { title: 'Selenium can\'t find elements', type: 'Question', status: 1, content: getMockupContent(), date: new Date(), tags: [] },
+        { title: 'How to configure GitHub?', type: 'Question', status: 2, content: getMockupContent(), date: new Date(), tags: [] },
+        { title: 'is ng-class faster to render views?', type: 'Note', status: -1, content: getMockupContent(), date: new Date(), tags: [] },
+        { title: 'Introduction to regular expressions', type: 'Article', status: -1, content: getMockupContent(), date: new Date(), tags: [] }
     ];
 
 }]);

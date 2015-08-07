b8130edc3290ddbde202690be3c7762fb3f7cc67
Ginoluis83 <gilu.carrascomori@gmail.com>
2015-08-07 15:59:39 -0500

    posts list added

--- iKnowIt/controllers/mainController.js
+++ iKnowIt/controllers/mainController.js
@@ -3,4 +3,11 @@
 
     $scope.message = '';
 
+    $scope.posts = [
+        { title: 'titulo 1', status: '', content: '', date: '' },
+        { title: 'titulo 2', status: '', content: '', date: '' },
+        { title: 'titulo 3', status: '', content: '', date: '' },
+        { title: 'titulo 4', status: '', content: '', date: '' }
+    ];
+
 }]);

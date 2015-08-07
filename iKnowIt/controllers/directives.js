281a382e0737a877018f0d9865a8ed45a6fbcd93
Guille <gleguia@gmail.com>
2015-08-07 15:48:24 -0500

    test

--- iKnowIt/controllers/directives.js
+++
@@ -1,8 +0,0 @@
-﻿angular.module('app.directives')
-.directive('test', function () {
-    return {
-        link: function (scope, element, attributes) {
-            alert('hadoken');
-        }
-    }
-});

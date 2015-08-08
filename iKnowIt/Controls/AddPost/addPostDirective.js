6f8a5395d00b3fbfb73612cea949896338ea400d
Guille <gleguia@gmail.com>
2015-08-07 20:19:42 -0500

    commit

--- iKnowIt/Controls/AddPost/addPostDirective.js
+++ iKnowIt/Controls/AddPost/addPostDirective.js
@@ -1,22 +1,21 @@
 app.directive('addPost', ['firebaseFactory', function (firebaseFactory) {
     return {
         restrict: "E",
-        templateUrl: "../Controls/AddPost/addPostTemplate.html",
-        scope: {
-           
-        },
+        templateUrl: "../Controls/AddPost/addPostTemplate.html",        
         link: function (scope, element, attributes) {
             scope.tags = [];
             scope.addTag = function () {
                 scope.tags.push(scope.newTag);
             }
-            var currentDate = new Date() + '';
+         
             scope.addPost = function () {
+                var currentDate = new Date().toDateString();
+                var content = element.find('#ql-editor-1 div')[0].innerHTML;
                 var newPost = {
-                    title: scope.title,
+                    title: scope.postTitle,
                     type: scope.type,
                     status: -1,
-                    content: 'holaaaa',
+                    content: content,
                     date: currentDate,
                     tags: scope.tags
                 };

269731d957c3129a5f275afb8f8a9992827467ec
Ginoluis83 <gilu.carrascomori@gmail.com>
2015-08-07 20:31:33 -0500

    Last chance

--- iKnowIt/controllers/mainController.js
+++ iKnowIt/controllers/mainController.js
@@ -1,5 +1,5 @@
 ﻿app.controller('mainController', ['$scope', 'firebaseFactory', function ($scope, firebaseFactory) {
-
+    $scope.inPost = false;
     var getMockupContent = function () {
         var content = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.';
 
@@ -26,7 +26,12 @@
 
     $scope.showPost = function (post) {
         $scope.selectedPost = post;
+        $scope.inPost = true;
     }
+    $scope.exitPost = function () {
+        $scope.inPost = false;
+    }
+
 }])
 
 .factory('firebaseFactory', ['$firebaseObject', function ($firebaseObject) {
